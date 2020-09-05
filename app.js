db982215cf2882bf02ae87d53e48fedc248c52e7
Atul <atulsingh1396@gmail.com>
2020-09-06 02:36:45 +0530

    db cnfig

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const config = require('./config/db')
+
+//connect to db
+mongoose.connect(config.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', ()=>{
+    console.log('Connected to MONGO');
+})
 
 const app = express();
 
@@ -11,7 +24,7 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req,res)=>{
-    res.send('Working')
+    res.send("Working");
 })
 
 //start server
