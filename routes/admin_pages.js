e293752714b753483addb34d2c385574be8def3b
Atul <atulsingh1396@gmail.com>
2020-09-06 03:48:38 +0530

    improving header, footer and nav. Adding add page tempalte

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -2,8 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+/*
+ * GET pages index
+ */
+router.get("/pages", (req, res) => {
   res.send("Admin");
 });
+
+/*
+ * GET add page
+ */
+router.get("/add-page", (req, res) => {
+  const title = "";
+  const slug = "";
+  const content = "";
+
+  res.render("admin/add_page", {
+    title,
+    slug,
+    content,
+  });
+});
+
 //exports
 module.exports = router;
