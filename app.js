239f8d9d0bf7a1111207f25af31df493b1659269
Atul <atulsingh1396@gmail.com>
2020-09-06 03:12:01 +0530

    added express session and flash message middleware

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
 const mongoose = require("mongoose");
 const config = require("./config/db");
 
-//connect to db
+// connect to db
 mongoose.connect(config.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -16,18 +17,39 @@ db.once("open", () => {
 
 const app = express();
 
+// express session middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+
+// express messages middlwware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-//set public
+// set public
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// set routes
+app.use("/", require("./routes/pages"));
+app.use("/admin", require("./routes/admin_pages"));
 
-//start server
+// start server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
